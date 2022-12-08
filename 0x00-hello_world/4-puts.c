196709966b9e3acf49840b0f051e358264dc5a82
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-08 15:42:35 -0800

    Write C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Return: Always 0 (Progress)
+ *Description: print "Programming is like building a multilingual puzzle"
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
