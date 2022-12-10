ae9cc7545a2b808c3cf56bc0128e1ebba15c1512
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-10 12:26:13 -0800

    All possible combination 2digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print all combination fo two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(b);
+		if (b != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		b++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
