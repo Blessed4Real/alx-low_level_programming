eaf4be4a07a149ad9f46d0b5590bf60ff24f5ead
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-10 10:48:45 -0800

    Print 1to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Via Blessing
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
