656d54902eb5941a334ec296419d1bf24ba49bc2
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-11 03:53:26 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - last combination for now
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int a, b, c, d;
+
+	a = 48;
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 48)
+		{
+			d = b + 1;
+			c = a;
+			while (c < 58)
+			{
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (a < 57 || b < 56 || c < 57 || d < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					d++;
+				}
+
+				c++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
