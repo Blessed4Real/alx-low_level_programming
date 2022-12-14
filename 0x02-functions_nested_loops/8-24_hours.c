0357215794535e50173240ce7ab9d4d931d7d35a
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-13 18:11:11 -0800

    Betty 24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day of jack Bauer
+ *
+ * Return: ...
+ */
+void jack_bauer(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; b <= 9; j++)
+		{
+			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
+			{
+				for (k = 0; k <= 5; k++)
+				{
+					for (m = 0; m <= 9; m++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(58);
+						_putchar(k + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
