9506db4aac748cb4bf1703fd30da72ce67f9789f
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-13 17:10:42 -0800

    Alphabetic Characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checking alphabetic characters
+ * @c:Alphabetic characters to be checked
+ * Return: 1 for alphabetic character (lower or upper) and 0 for anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
