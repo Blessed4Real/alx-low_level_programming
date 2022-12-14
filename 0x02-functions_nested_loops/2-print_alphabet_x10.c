f19ee40ac8c431e4273fdc3bd9af2099d34a0029
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-13 16:25:18 -0800

    Print alphabet 10t

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times in lowercase and a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int a = 0;
+
+	while (a <= 9)
+	{
+		for (c <= 'z'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		a++;
+	}
+}
