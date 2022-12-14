a8a140850cdea2dec550c7bbe0d94bc6de4d6202
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-13 17:34:41 -0800

    integer output

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - the absolute output value of an integer input
+ * @j: to check integer input
+ *
+ * Return: Always j
+ */
+
+
+int _abs(int j)
+{
+	if (j >= 0)
+		return (j);
+
+	else
+	{
+		j *= -1;
+		return (n);
+	}
+}
