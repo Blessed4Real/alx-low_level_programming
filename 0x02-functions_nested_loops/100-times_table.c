ef8c9a623851749ceaeffea584e0ca70acdff5a9
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-13 20:07:32 -0800

    100t tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,6 +39,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 100) + '0');
+					_putchar((c / 10) % '0');
 					_putchar((c % 10) + '0');
 				}
 			}
