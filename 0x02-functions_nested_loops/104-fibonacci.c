c1d614865fb771b601b75c21cd51ecd6b2c97539
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-14 19:55:35 -0800

    Print Fibonacci 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,36 +7,31 @@
  */
 int main(void)
 {
-	unsigned long int a, b, c, a1, a2, c1, c2;
+	unsigned long int bl1 = 0, mw1 = 1, bl2 = 0, mw2 = 2;
+	unsigned long int doll1, doll2, doll3;
+	int count;
 
-	a = 1;
-	c = 2;
-
-	printf("%lu", b);
-
-	for (a = 1; a < 91; a++)
+	printf("%lu, %lu, ", mw1, mw2);
+	for (count = 2; count < 98; count++)
 	{
-		printf(", %lu", c);
-		c = c + b;
-		b = c - b;
+		if (mw1 + mw2 > LARGEST || bl2 > 0 || bl1 > 0)
+		{
+			doll1 = (mw1 + mw2) / LARGEST;
+			doll2 = (mw1 + mw2) % LARGEST;
+			doll3 = bl1 + bl2 + doll1;
+			bl1 = bl2, bl2 = doll3;
+			mw1 = mw2, mw2 = doll2;
+			printf("%lu%010lu", bl2, mw2);
+		}
+		else
+		{
+			doll2 = mw1 + mw2;
+			mw1 = mw2, mw2 = doll2;
+			printf("%lu", mw2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-
-	b1 = b / 1000000000;
-	b2 = b % 1000000000;
-	c1 = c / 1000000000;
-	c2 = c % 1000000000;
-
-	for (a = 92; a < 99; ++a)
-	{
-		printf(", %lu", c1 + (c2 / 1000000000));
-		printf("%lu", c2 % 1000000000);
-		c1 = c1 + b1;
-		b1 = c1 - b1;
-		c2 = c2 + b2;
-		b2 = c2 - b2;
-	}
-
 	printf("\n");
-
 	return (0);
 }
