0db01419f943c324355128ab4ab5eb2db2f56a89
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-19 14:06:49 -0800

    Convert the String of an Int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the pointer to convert
+ * Return: An integer
+ */
+
+int _atoi(char *s)
+{
+	int b = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[b])
+	{
+		if (s[b] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[b] >= 48 && s[b] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[b] - '0');
+			b++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		b++;
+	}
+
+	ni *= min;
+	return (ni);
+}
