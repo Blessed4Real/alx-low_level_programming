6df5debebe5d1b8885a747c00c86bfc8fb68c89d
Blessed4Real <blessingirenhi.bi@gmail.com>
2022-12-22 09:29:31 -0800

    Compares string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,8 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if s1 and s2 are equal and any number if not
+ * Return: 0 if s1 and s2 are equal
+ *  and another number if not
  */
 
 int _strcmp(char *s1, char *s2)
@@ -18,6 +19,6 @@ int _strcmp(char *s1, char *s2)
 		b = *(s1 + a) - *(s2 + a);
 		a++;
 	}
-
+	
 	return (b);
 }
