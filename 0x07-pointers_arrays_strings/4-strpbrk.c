8db34fcf63ef2ebafacd2c533e6d8cd7169aff74
Blessed4Real <blessingirenhi.bi@gmail.com>
2023-01-04 03:52:36 -0800

    Bytes that matches

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: first string
+ * @accept: accept, or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (a = 0; *(s + a) != '\0'; a++)
+	{
+		for (b = 0; *(accept + b) != '\0'; b++)
+		{
+			if (*(s + a) == *(accept + b))
+				return (s + a);
+		}
+	}
+	return ('\0');
+}
